bc9b665f4405dce2601a1bc102c951c29768f81d
luitor238 <luitor238@alu.centroafuera.es>
2024-01-04 12:28:48 +0100

    Añadida pantalla de carga Añadido LogIn a falta de la funcion Añadido SingIn

---
+++ app/src/main/java/com/example/proyectofinalgrado/ChargingScreenActivity.kt
@@ -0,0 +1,19 @@
+package com.example.proyectofinalgrado
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ChargingScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_charging_screen)
+
+        var duracion = 2500 as Long
+
+        android.os.Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }, duracion)
+    }
+}
